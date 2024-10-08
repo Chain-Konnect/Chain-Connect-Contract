bb2559edf44e12809e692a93e92e8604fcfcb47d
drex007 <vykeoj@gmail.com>
2024-10-08 02:42:23 +0100

    contract edit

--- contracts/ChainConnectSocial.sol
+++ contracts/ChainConnectSocial.sol
@@ -34,6 +34,7 @@ contract ChainConnectSocial {
         address userAddress;
         string profileImage;
         uint256 earnedAmount;
+        uint256 igniteAmount;
         bool status;
         string profile_name;
         uint256 followerCount;
@@ -66,7 +67,7 @@ contract ChainConnectSocial {
         isAdmin[msg.sender] = true;
     }
 
-    function createAPost(string memory usersPost, string memory imageOne, string memory imageTwo) public payable returns(uint) {
+    function createAPost(string memory usersPost, string memory imageOne, string memory imageTwo) public returns(uint) {
         require(users[msg.sender].status,"Status is inactive");
         uint256 newPostId = postId++;
         Post storage post = posts[newPostId];
@@ -77,23 +78,23 @@ contract ChainConnectSocial {
         post.imageTwo = imageTwo;
         post.createdAt = block.timestamp;
         postIds.push(newPostId);
-        tokenAddress.transfer(msg.sender,earningsPerInteraction);
+        users[msg.sender].earnedAmount += earningsPerInteraction;
         return newPostId;
     }
 
     
 
-    function likeAPost(uint _postId) payable  public {
+    function likeAPost(uint _postId)  public {
         require(!postLikes[_postId][msg.sender], "Already liked this post");
         require(users[msg.sender].status,"Status is inactive");
         postLikes[_postId][msg.sender] = true;
         posts[_postId].likeCount++;
-        tokenAddress.transfer(msg.sender,earningsPerInteraction);
+        users[msg.sender].earnedAmount += earningsPerInteraction;
         emit LikeATweet(posts[_postId].author, _postId,msg.sender, block.timestamp);
 
     }
 
-    function commentOnPost(uint _postId, string memory comment, string memory imageOne, string memory imageTwo) public payable {
+    function commentOnPost(uint _postId, string memory comment, string memory imageOne, string memory imageTwo) public {
         require(users[msg.sender].status,"Status is inactive");
         postComments[_postId].push(Comment({
             author: msg.sender,
@@ -104,17 +105,18 @@ contract ChainConnectSocial {
             createdAt: block.timestamp
         }));
         posts[_postId].commentCount++;
-        tokenAddress.transfer(msg.sender,earningsPerInteraction);
+        users[msg.sender].earnedAmount += earningsPerInteraction;
         emit CommentOnATweet(posts[_postId].author, _postId, msg.sender,block.timestamp);
     }
 
-    function igniteAPost(uint _postId, uint256, address payable  _author)  public payable  {
+    function igniteAPost(uint _postId, address payable  _author)  public payable  {
         require(users[msg.sender].status,"Status is inactive");
         require(msg.value > 0, "Value  must exceed 0");
         (bool sent,) =_author.call{value:msg.value}("");
         require(sent, "Ignite Failed");
         posts[_postId].igniteCount += 1;
-        users[_author].earnedAmount += msg.value;
+        users[_author].igniteAmount += msg.value;
+        users[msg.sender].earnedAmount += earningsPerInteraction;
         emit IgniteATweet(msg.sender, _postId, msg.value, _author,block.timestamp);
       
     }
@@ -144,8 +146,8 @@ contract ChainConnectSocial {
     }
 
     function addUser ()   public {
-    require(users[msg.sender].userAddress == address(0),"User not found");
-    users[msg.sender] = User(userId,msg.sender,"",0,true,"",0,0);
+    require(users[msg.sender].userAddress == address(0),"User already active");
+    users[msg.sender] = User(userId,msg.sender,"",0,0,true,"",0,0);
     userId++;
       
     }
@@ -154,23 +156,7 @@ contract ChainConnectSocial {
         user = users[_address];
         return user;
     }
-    
-    function getRecentPosts(uint pageNumber, uint pageSize) public view returns (Post[] memory) {
-    require(pageSize > 0, "Page size must be greater than 0");
-    
-    uint totalPosts = postIds.length;
-    uint startIndex = totalPosts > pageSize * pageNumber ? totalPosts - pageSize * pageNumber : 0;
-    uint endIndex = totalPosts > startIndex + pageSize ? startIndex + pageSize : totalPosts;
 
-    uint numberOfPosts = endIndex - startIndex;
-    Post[] memory recentPosts = new Post[](numberOfPosts);
-
-    for (uint i = 0; i < numberOfPosts; i++) {
-        recentPosts[i] = posts[postIds[startIndex + i]];
-    }
-
-    return recentPosts;
-}
 
    function updateAvatar (string memory avatarCID)   public returns(bool) {
     users[msg.sender].profileImage = avatarCID;
@@ -178,13 +164,13 @@ contract ChainConnectSocial {
     
    }
 
-
-   function depositToSocialFi(uint256 _amount)   public {
-    require(msg.sender == admin, "Not an admin");
-    require(tokenAddress.transferFrom(msg.sender, address(this), _amount));
-
-
+   function updateProfileName  (string memory profileName)   public returns(bool) {
+    users[msg.sender].profile_name = profileName;
+    return true;
+    
    }
+
+
 
    function socialFiBalance () public view returns(uint256){
     uint256 balance = tokenAddress.balanceOf(address(this));
@@ -244,6 +230,13 @@ function getFollowers(address _user) public view returns (address[] memory) {
 
 function getFollowing(address _user) public view returns (address[] memory) {
     return following[_user];
+}
+
+function  claimRewards () public payable{
+    require(users[msg.sender].earnedAmount > 0, "Amount must be greater than 0");
+    tokenAddress.transfer(msg.sender, users[msg.sender].earnedAmount);
+    users[msg.sender].earnedAmount = 0;
+
 }
 
 
--- contracts/ChainConnectToken.sol
+++ contracts/ChainConnectToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.3 <0.9.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ChainConnectToken is ERC20, ERC20Burnable,ERC20Pausable, Ownable  {
     constructor(uint256 initialSupply) ERC20("ChainConnectToken", "CCT") Ownable(msg.sender) {
-        uint256 _totalSupply = initialSupply * 10 ** 18;
+        uint256 _totalSupply = initialSupply * 10 ** 6;
         _mint(msg.sender, _totalSupply);
     }
     function burn(uint256 value) public override  virtual {
